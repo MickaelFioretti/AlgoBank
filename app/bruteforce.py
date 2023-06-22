1cfc41a5b6ef2094888880b881f64314b8cc59a2
Mickael Fioretti <mickael.fioretti@drotek.com>
2023-06-22 19:03:47 +0200

    Feat : save work

--- app/bruteforce.py
+++ app/bruteforce.py
@@ -1,5 +1,4 @@
 import json
-from time import sleep
 
 # Variables
 """
@@ -15,7 +14,7 @@ action_list: list = []
 investissement: list = []
 
 # Read JSON file
-with open('./action.json') as f:
+with open("./action.json") as f:
     data: list = json.load(f)
 
 
@@ -23,23 +22,24 @@ with open('./action.json') as f:
 def brute_force() -> None:
     for i in range(len(data)):
         action_list.append(data[i])
-        if len(action_list) > 2:
-            # aditionner tous les couts par action de la liste
-            cout: int = sum(
-                [action['cout_par_action'] for action in action_list])
-            # aditionner tous les benefices de la liste
-            benefice: int = sum(
-                [action['benefice'] for action in action_list])
-            # si le cout est inferieur ou egal a 500
-            if cout <= 500:
-                # afficher le cout et le benefice
-                print(f'cout: {cout} benefice: {benefice}')
-                # faire une variable avec les actions et le benefice
-                investissement.append({
-                        'actions': action_list,
-                        'benefice': benefice
-                    })
-                print(investissement)
+        # aditionner tous les couts par action de la liste
+        cout: int = sum([action["cout_par_action"] for action in action_list])
+        # aditionner tous les benefices de la liste
+        benefice: int = sum([action["benefice"] for action in action_list])
+        # si le cout est inferieur ou egal a 500
+        if cout <= 500:
+            # afficher le cout et le benefice
+            # print(f"cout: {cout} benefice: {benefice}")
+            # faire une variable avec les actions et le benefice
+            print(action_list)
+            investissement.append(
+                [
+                    "essai n°: " + str(i + 1),
+                    "actions: " + str(action_list),
+                    "benefice: " + str(benefice),
+                ]
+            )
+    print(investissement[0])
 
 
 brute_force()
