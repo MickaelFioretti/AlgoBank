05d07d71aacd4f425ce55875860bbab38eb5b36a
Mickael <fiorettimickael1@gmail.com>
2023-06-23 11:10:15 +0200

    Feat : Bruteforce is Done !

--- app/bruteforce.py
+++ app/bruteforce.py
@@ -1,6 +1,6 @@
 import json
 
-# Variables
+# Read JSON file
 """
     {
         "actions": "Action-1",
@@ -8,38 +8,57 @@ import json
         "benefice": 5
     },
 """
-
-data: list = []
-action_list: list = []
-investissement: list = []
-
-# Read JSON file
 with open("./action.json") as f:
-    data: list = json.load(f)
+    actions: list = json.load(f)
+
+# variables
+valid_combinations: list = []
 
 
-# Brute force
-def brute_force() -> None:
-    for i in range(len(data)):
-        action_list.append(data[i])
-        # aditionner tous les couts par action de la liste
-        cout: int = sum([action["cout_par_action"] for action in action_list])
-        # aditionner tous les benefices de la liste
-        benefice: int = sum([action["benefice"] for action in action_list])
-        # si le cout est inferieur ou egal a 500
-        if cout <= 500:
-            # afficher le cout et le benefice
-            # print(f"cout: {cout} benefice: {benefice}")
-            # faire une variable avec les actions et le benefice
-            print(action_list)
-            investissement.append(
-                [
-                    "essai n°: " + str(i + 1),
-                    "actions: " + str(action_list),
-                    "benefice: " + str(benefice),
-                ]
-            )
-    print(investissement[0])
+# Function to generate combinations
+def generate_combinations(actions: list, r, combination=[]) -> None:
+    """
+    Generate all combinations of actions
+    """
+    if r == 0:
+        total_cost: int = sum(
+            [action["cout_par_action"] for action in combination]
+        )
+        if total_cost == 500:
+            valid_combinations.append(combination)
+        return
+
+    for i in range(len(actions)):
+        new_combination = combination + [actions[i]]
+        generate_combinations(actions[i + 1 :], r - 1, new_combination)
 
 
-brute_force()
+# Script
+for r in range(1, len(actions) + 1):
+    generate_combinations(actions, r)
+
+
+"""
+    Ajoute les combinaisons valides dans un fichier JSON
+"""
+# Creer une structure de donnees
+results = {"combinations": []}
+
+# Ajoute les combinaisons valides au resultat
+for combination in valid_combinations:
+    combination_data = [
+        {"action": action["actions"], "cost": action["cout_par_action"]}
+        for action in combination
+    ]
+    print(
+        "Total cost: ",
+        sum([action["cout_par_action"] for action in combination]),
+    )
+    results["combinations"].append(combination_data)
+
+# Ecrit le resultat dans un fichier JSON
+with open("./result.json", "w") as f:
+    json.dump(results, f, indent=4)
+
+
+print("Fin du script !")
